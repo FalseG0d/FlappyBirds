2dfc39589db55243f9f561cfc715130b931e78a7
Apoorv Garg <u.garg.10@gmail.com>
2020-03-18 13:31:26 +0530

    Bird Is Jumping

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+
+public class Bird : MonoBehaviour
+{
+    Rigidbody2D birdRigidbody2D;
+    const float JUMP_AMOUNT = 100f;
+    void Awake()
+    {
+        birdRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) 
+        {
+            Jump();
+        }
+        
+    }
+
+    void Jump()
+    {
+        birdRigidbody2D.velocity = Vector2.up * JUMP_AMOUNT;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        //CMDebug.TextPopMouse("Dead!!");
+
+    }
+}
---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+
+    static GameAssets instance;
+
+    public static GameAssets GetInstance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public Sprite pipeHeadSprite;
+
+}
